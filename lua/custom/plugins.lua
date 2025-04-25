5d190d515ae4134b52bb34e9aa60f031c0f710d7
Bruno Braga <braga.bruno81@gmail.com>
2025-04-25 10:48:49 -0500

    feat: add additional plugins including nvim-autopairs, todo-comments, and lspkind

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -42,7 +42,88 @@ local plugins = {
   {
     "hrsh7th/nvim-cmp",
     opts = overrides.cmp,
-  }
+  },
+  
+  -- Additional plugins
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "szw/vim-maximizer",
+    lazy = true,
+    cmd = "MaximizerToggle",
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {"html", "javascript", "typescript", "javascriptreact", "typescriptreact", "svelte", "vue", "tsx", "jsx", "xml"},
+    dependencies = "nvim-treesitter/nvim-treesitter",
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "BufReadPre",
+    opts = {},
+  },
+  {
+    "onsails/lspkind.nvim",
+    lazy = true,
+    dependencies = "hrsh7th/nvim-cmp",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    version = "v2.*",
+    build = "make install_jsregexp",
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
+  {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+      require("Comment").setup({
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      })
+    end,
+  },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
 
 return plugins
