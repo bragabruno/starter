bffd69d12a7a113660fe1bdf10cb7ac9d656e130
Bruno Braga <braga.bruno81@gmail.com>
2025-04-25 10:29:40 -0500

    feat: update plugin configuration to include Mason and LSP support

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -2,6 +2,28 @@ local overrides = require "custom.configs.overrides"
 
 local plugins = {
   {
+    "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonUpdate" },
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+      },
+    },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = overrides.treesitter,
   },
